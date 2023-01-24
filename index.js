79a051c6b2f205e480e908548eb0bcbe2b3443d4
Alberto Macis <bebo.macis@gmail.com>
2023-01-24 17:58:13 +0100

    add

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// esercizio 1
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>esercizio 1<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 class Person {
   constructor(name, age) {
     this.name = name;
@@ -7,9 +7,25 @@ class Person {
 
   static elder(person1, person2) {
     if (person1.age > person2.age) {
-      console.log(person1.name + " è più grande di " + person2.name);
+      console.log(
+        person1.name +
+          " ha " +
+          person1.age +
+          " anni, quindi è più grande di " +
+          person2.name +
+          " che ne ha " +
+          person2.age
+      );
     } else {
-      console.log(person2.name + " è più grande di " + person1.name);
+      console.log(
+        person2.name +
+          " ha " +
+          person2.age +
+          " anni, quindi è più grande di " +
+          person1.name +
+          " che ne ha " +
+          person1.age
+      );
     }
   }
 }
@@ -20,9 +36,11 @@ let p3 = new Person("Giulia", 22);
 
 Person.elder(p2, p1);
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>esercizio 2<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>esercizio 2<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-myButton = document.getElementById("myButton");
+myButton1 = document.getElementById("myButton1");
+myButton2 = document.getElementById("myButton2");
+myButton3 = document.getElementById("myButton3");
 myUl = document.querySelector("ul");
 
 const newArray = [
@@ -38,6 +56,19 @@ const newArray = [
   "Valentina",
   "Simone",
   "Roberta",
+  "Monica",
+  "Simone",
+  "Giulia",
+  "Francesca",
+  "Stefano",
+  "genovefo",
+  "guglielma",
+  "ermegilda",
+  "mariateresa",
+  "Valerio",
+  "Valentina",
+  "Simone",
+  "Roberta",
 ];
 
 let pageSize = 10;
@@ -49,12 +80,44 @@ class Pagination {
   }
 }
 
-myButton.onclick = function () {
+myButton1.onclick = function () {
+  myUl.innerText = "";
   let newPagination = new Pagination(newArray, pageSize);
+
   for (let i = 0; i < newPagination.pageSize; i++) {
     const element = newPagination.items[i];
-    let li = document.createElement("li");
-    li.innerText = element;
-    myUl.appendChild(li);
+    if (element) {
+      let li = document.createElement("li");
+      li.innerText = element;
+      myUl.appendChild(li);
+    }
+  }
+};
+
+myButton2.onclick = function () {
+  myUl.innerText = "";
+  let newPagination = new Pagination(newArray, pageSize);
+
+  for (let i = 0; i < newPagination.pageSize; i++) {
+    const element = newPagination.items[i + 10];
+    if (element) {
+      let li = document.createElement("li");
+      li.innerText = element;
+      myUl.appendChild(li);
+    }
+  }
+};
+
+myButton3.onclick = function () {
+  myUl.innerText = "";
+  let newPagination = new Pagination(newArray, pageSize);
+
+  for (let i = 0; i < newPagination.pageSize; i++) {
+    const element = newPagination.items[i + 20];
+    if (element) {
+      let li = document.createElement("li");
+      li.innerText = element;
+      myUl.appendChild(li);
+    }
   }
 };
